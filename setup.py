c9ae1d13e7e740170541a982550aba41ff1c2268
Joseph Guidry <josephguidry@utexas.edu>
2021-06-23 17:04:58 -0500

    updated description and url

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name=”reciPY”,
-    version=”0.1”,
+    name="reciPY",
+    version="0.1",
+    description='reciPY! The Python package for your kitchen.',
+    url='https://github.com/astrojoeg/reciPY',
     packages=find_packages()
     )
